bfb28034c1a5796b46be75678c128768c2984586
Aditya Ekka <adityaekka2003@gmail.com>
2023-12-27 12:26:59 +0530

    create the test api route

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from '../routes/user_route.js';
 dotenv.config();
+
 mongoose.connect(process.env.MONGO)
     .then(() => {
         console.log("Connect Successful");
@@ -12,6 +14,8 @@ mongoose.connect(process.env.MONGO)
 
 const PORT = 4000;
 const app = express();
+
+app.use('/api/user',userRouter);
 
 app.listen(PORT,()=>{
     console.log(`Listening to localhost ${PORT}`);
