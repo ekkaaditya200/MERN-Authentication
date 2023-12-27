0a3c18647f6d2f05e478381006fa61443b6efbf6
Aditya Ekka <adityaekka2003@gmail.com>
2023-12-27 14:40:00 +0530

    add a middleware and function to handle errors

--- server/api/index.js
+++ server/api/index.js
@@ -18,7 +18,18 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes)
+app.use('/api/auth',authRoutes);
+
+//!Middleware
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode,
+    })
+})
 
 app.listen(PORT,()=>{
     console.log(`Listening to localhost ${PORT}`);
--- server/controllers/auth_controller.js
+++ server/controllers/auth_controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user_model.js'
 import bcryptjs from 'bcryptjs';
-export const signup = async (req,res) => {
+import { errorHandler } from '../utls/error.js';
+export const signup = async (req,res,next) => {
     const {username,email,password}=req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({username,email,password:hashedPassword});
@@ -9,6 +10,8 @@ export const signup = async (req,res) => {
         res.status(201).json({message:"User created successfully"});
     }catch(error)
     {
-        res.status(400).send(error);
+        // res.status(400).send(error);
+        // next(errorHandler(300,"Some went wrong")) //custom error
+        next(error)
     }
 };
