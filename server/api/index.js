adccb95acfa2d0883e8995342e7cf38bd6b2befa
Aditya Ekka <adityaekka2003@gmail.com>
2023-12-27 11:59:55 +0530

    connect to mongodb

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
-const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connect Successful");
+    })
+    .catch((error) => {
+        console.log("Error", error);
+    })
+
 const PORT = 4000;
+const app = express();
+
 app.listen(PORT,()=>{
-    console.log(`Server listening on port ${PORT}`)
+    console.log(`Listening to localhost ${PORT}`);
 })
