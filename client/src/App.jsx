e3b05e8b492bd580ec6162f49bc5aeb41e83ade5
Aditya Ekka <adityaekka2003@gmail.com>
2023-12-28 22:39:28 +0530

    update header component and make profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
-import { BrowserRouter, Routes,Route } from "react-router-dom"
-import Header from "./components/header"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header";
 import Home from "./pages/Home"
 import About from "./pages/About"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
+import PrivateRoute from "./components/PrivateRoute"
 function App() {
   return (
     <BrowserRouter>
-    <Header></Header>
-    <Routes>
-      <Route path='/' element={<Home></Home>}></Route>
-      <Route path='/about' element={<About></About>}></Route>
-      <Route path='/sign-in' element={<SignIn></SignIn>}></Route>
-      <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
-      <Route path='/profile' element={<Profile></Profile>}></Route>
-    </Routes>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/sign-in' element={<SignIn></SignIn>}></Route>
+        <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile></Profile>}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 const Header = () => {
+  const {currentUser} = useSelector(state=>state.user);
   return (
     <div className="bg-slate-200">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -7,7 +9,15 @@ const Header = () => {
             <ul className="flex gap-4">
                 <Link to='/'>Home</Link>
                 <Link to='/about'>About</Link>
-                <Link to='/sign-in'>Sign In</Link>
+                <Link to='/profile'>
+                 {currentUser ? (
+                  <img src={currentUser.profilePicture} alt="profile" className="h-7 w-7 rounded-full object-cover"/>
+                 ):(
+                  <li>
+                  Sign In
+                   </li>
+                 )}
+                </Link>
             </ul>
         </div>
     </div>
