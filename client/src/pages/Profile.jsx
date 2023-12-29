acef97144102eec8dd1198cafe0faee670c07f26
Aditya Ekka <adityaekka2003@gmail.com>
2023-12-29 12:59:24 +0530

    add upload image functionality to firebase

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,24 +1,106 @@
 import { useSelector } from "react-redux"
-
+import { useEffect, useRef, useState } from "react";
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import app from "../firebase";
 const Profile = () => {
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercnt] = useState(0);
+  const [imageError, setImageError] = useState(false);
+  const [formData, setFormData] = useState({});
   const { currentUser } = useSelector(state => state.user);
+  const fileRef = useRef(null);
+
+  const handleImageUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercnt(Math.round(progress));
+
+      },
+      (error) => {
+        setImageError(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, profilePicture: downloadURL }));
+      }
+    );
+  }
+
+  useEffect(() => {
+    if (image) {
+      console.log(image.name);
+      handleImageUpload(image);
+    }
+  }, [image])
+
   return (
     <div className="p-3 max-w-lg mx-auto">
+
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+
       <form className="flex flex-col gap-5">
-        <img src={currentUser.profilePicture} alt='profile' className="h-24 w-24 self-center rounded-full cursor-pointer mt-2"></img>
-        <input defaultValue={currentUser.username} type="text" name="username" id="username" placeholder="Username" className="bg-slate-100 rounded-lg p-3" />
-        <input defaultValue={currentUser.email} type="email" name="email" id="email" placeholder="Email" className="bg-slate-100 rounded-lg p-3" />
-        <input type="password" name="password" id="password" placeholder="Password" className="bg-slate-100 rounded-lg p-3" />
+        <input
+          type='file'
+          ref={fileRef}
+          hidden
+          accept='image/*'
+          onChange={(e) => setImage(e.target.files[0])} />
+
+        <img
+          src={formData.profilePicture || currentUser.profilePicture}
+          alt='profile'
+          onClick={() => fileRef.current.click()}
+          className="h-24 w-24 self-center rounded-full cursor-pointer mt-2"></img>
+
+        <p>
+          {imageError ? (
+            <span className="text-red-700">Error Uploading Image (file size must be less than 2 MB)</span>
+          ) : (
+            imagePercent > 0 && image < 100 ? (
+              <span className="text-slate-700">{`Uploading:${imagePercent}%`}</span>
+            ) : (imagePercent === 100 ? (<span className="text-green-700">Image Uploaded Successfully</span>) : '')
+          )}
+        </p>
+
+        <input
+          defaultValue={currentUser.username}
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Username" className="bg-slate-100 rounded-lg p-3" />
+
+        <input
+          defaultValue={currentUser.email}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email" className="bg-slate-100 rounded-lg p-3" />
+
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 rounded-lg p-3" />
+
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Update</button>
+
       </form>
+
       <div className="flex justify-between mt-5 font-bold">
-        <sapan className='text-red-600 cursor-pointer'>Delete Account</sapan>
-        <sapan className='text-red-600 cursor-pointer'>Sign Up</sapan>
+        <span className='text-red-600 cursor-pointer'>Delete Account</span>
+        <span className='text-red-600 cursor-pointer'>Sign Up</span>
       </div>
+
     </div>
   )
 }
 
 export default Profile
-Profile
